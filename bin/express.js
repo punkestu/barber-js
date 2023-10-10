667f2463f09d602151bbaa1773963a791c555b1a
Dobberman <pangestubima89@gmail.com>
2023-10-10 15:37:34 +0700

    feature(barber) : barber's schedule getter

--- bin/express.js
+++ bin/express.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const auth = require("../src/app/auth")();
 const authMid = require("../src/middleware/auth")(auth.repo);
-const shift = require("../src/app/shift")(authMid);
-const barber = require("../src/app/barber")(auth.repo, shift.repo, authMid);
-
 app.use("/person", auth.http);
+
+const shift = require("../src/app/shift")(authMid);
 app.use("/shift", shift.http);
+
+const barber = require("../src/app/barber")(auth.repo, shift.repo, authMid);
 app.use("/barber", barber.http);
 
 app.listen(3000, ()=>console.log("listen at :3000"));
--- src/app/barber/usecase/barber.js
+++ src/app/barber/usecase/barber.js
@@ -53,6 +53,10 @@ class Barber {
         barber.active = !barber.active;
         return this.#repo.Save(barber);
     }
+
+    async GetSchedule({day}) {
+        return this.#repo.LoadByBarber({day});
+    }
 }
 
 module.exports = Barber;
