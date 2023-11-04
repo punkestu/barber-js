895bf0e7447e3cd661fe5d64a50bf542a6a520fe
Dobberman <pangestubima89@gmail.com>
2023-11-04 12:09:46 +0700

    fix(app) : fixing db pool and db query

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-require("./src/lib/db").Init("mysql://bima:@localhost:3306/barber");
+require("dotenv").config();
+require("./src/lib/db").Init({
+    host     : 'localhost',
+    user     : process.env.DB_USERNAME,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
+});
 require("./bin/express");
